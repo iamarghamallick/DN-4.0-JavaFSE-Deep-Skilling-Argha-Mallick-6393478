bd3becb8ff2a4811584d462ddecfdf72794d99b9
Argha Mallick <arghamallick5@gmail.com>
2025-07-23 21:26:45 +0530

    ReactJs-HOL-2

---
+++ Week 6/studentapp/src/App.js
@@ -0,0 +1,16 @@
+import Home from './components/Home';
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="container">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
