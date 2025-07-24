4c7fac61d6df9c35fd0c7f890a0beb7366e81ff0
Argha Mallick <arghamallick5@gmail.com>
2025-07-24 16:10:50 +0530

    shopping app

---
+++ Week 6/shoppingapp/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import OnlineShopping from './OnlineShopping';
+import './App.css'; // Importing CSS for styling
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <OnlineShopping />
+  </React.StrictMode>
+);
